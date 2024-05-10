95e698577ef81d402bb574e89ee71e65e13d4ad7
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-10 12:43:15 +0300

    Submitting Putting it All Together:Components and Props

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-
-
 function Article({ title, date = "January 1, 1970", preview }) {
   return (
     <article>
       <h3>{title}</h3>
-      <small>
-        {date}
-        {/* {date}.{emojis} {minutes} min read */}
-      </small>
+      <small>{date}</small>
       <p>{preview}</p>
     </article>
   );
